4b1e0983802c1783f5050fe752c30edd634a7577
Daniel Fernandez <dfernandez@certiorem.com>
2020-02-20 15:53:41 +0100

    primer commit configuration server

---
+++ springboot-config-server-service/src/main/java/com/sucede/sb/app/config/SpringbootConfigServerServiceApplication.java
@@ -0,0 +1,15 @@
+package com.sucede.sb.app.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class SpringbootConfigServerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootConfigServerServiceApplication.class, args);
+	}
+
+}
